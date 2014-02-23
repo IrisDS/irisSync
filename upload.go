c2e1185bd3fa98b08f54968faabe98c9baad3a89
wmak <william.mak@hotmail.ca>
2014-02-22 19:40:41 -0500

    initial commit for upload5

---
+++ upload.go
@@ -0,0 +1,31 @@
+package irisSync
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+/* based on:
+* https://github.com/ljgww/web_server_example_in_Go_-golang-
+ */
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		log.Fatal("wrong request method")
+	}
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	temp, err := ioutil.TempFile("./images", "image-")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer temp.Close()
+	_, err = io.Copy(temp, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
